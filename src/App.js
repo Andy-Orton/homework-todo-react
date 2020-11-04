20b9ae1a868146e23a47bf63426116cd83004102
Andy-Orton <andyorton13@gmail.com>
2020-11-04 11:43:05 -0700

    added files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Homework from './components/homework-todo'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Homework></Homework>
+      </div>
+    </div>
+  );
+}
+
+export default App;
